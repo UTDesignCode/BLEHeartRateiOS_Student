c3f490128cbc6cd2f51712e51b6ec0d99d8d1853
Khan Mohammad <mxk126130@spnc78760.utdallas.edu>
2017-08-02 13:15:00 -0500

    Initial Commit

---
+++ BLEHeartRateiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLEHeartRateiOS
+//
+//  Created by Khan, Mohammad on 8/2/17.
+//  Copyright © 2017 Khan, Mohammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLEHeartRateiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLEHeartRateiOS
+//
+//  Created by Khan, Mohammad on 8/2/17.
+//  Copyright © 2017 Khan, Mohammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
